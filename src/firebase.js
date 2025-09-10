5f595600d446fb0c92a97b216619186b354c524c
Abuthahir31 <syedabuthahirabbas53@gmail.com>
2025-09-10 10:29:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpQ9di-h_rv17djknnzQsE-9EXCpTJlIk",
-  authDomain: "myeio-4b580.firebaseapp.com",
-  projectId: "myeio-4b580",
-  storageBucket: "myeio-4b580.firebasestorage.app",
-  messagingSenderId: "873332102535",
-  appId: "1:873332102535:web:61aaa784f8513ec7fd6f22",
-  measurementId: "G-2J9Y7BMXDK"
+// your firebase key
 };
 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider(); 
+
 
 export { auth, googleProvider };
